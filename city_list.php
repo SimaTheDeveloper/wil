3892196940bcebb45208af987eb050bb20e54280
Mosima Kobe <118491083+SimaTheDeveloper@users.noreply.github.com>
2022-11-18 02:31:25 +0200

    Add files via upload

---
+++ city_list.php
@@ -0,0 +1,7 @@
+
+<option value="">City...</option>
+<option value="Tshwane">Tshwane</option>
+<option value="Midrand">Midrand</option>
+<option value="Joburg">Joburg</option>
+<option value="Sandton">Sandton</option>
+<option value="Polokwane">Polokwane</option>
